ada1e7a35b1675f3fbbd2751e669e6cc0bbf4645
psalmistcom <dadewale9@gmail.com>
2023-01-02 23:15:57 +0100

    seed listings

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,30 +2,10 @@
 
 namespace App\Models;
 
-class Listing{
-    public static function all() 
-    {
-        return [
-            [
-                'id'=> 1,
-                'title'=> 'Listing One',
-                'description'=> 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt praesentium, distinctio ducimus optio consequatur corrupti, rem voluptatem iste dolores veritatis amet accusamus, dignissimos obcaecati vitae! Tenetur adipisci quis quidem. Dolore?'
-            ],
-            [
-                'id'=> 2,
-                'title'=> 'Listing Two',
-                'description'=> 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt praesentium, distinctio ducimus optio consequatur corrupti, rem voluptatem iste dolores veritatis amet accusamus, dignissimos obcaecati vitae! Tenetur adipisci quis quidem. Dolore?'
-            ]
-        ];
-    }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-    public static function find($id){
-        $listings = self::all();
-
-        foreach ($listings as $listing) {
-            if ($listing['id'] == $id) {
-                return $listing;
-            }
-        }
-    }
+class Listing extends Model
+{
+    use HasFactory;
 }
