652def6e62a85f21a8e01654373148b78db83e07
psalmistcom <dadewale9@gmail.com>
2023-01-03 00:56:22 +0100

    style single

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,20 +1,14 @@
 @extends('layout')
 
 @section('content')
-
+@include('partials._hero')
+@include('partials._search')
 <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
 
 @unless (count($listings) == 0)
 
     @foreach ($listings as $listing)
-
-        {{-- <h3> 
-            <a href="/listings/{{$listing['id']}}"> 
-                {{$listing['title']}} 
-            </a> 
-        </h3>
-        <p>{{$listing['description']}}</p> --}}
-
+       
         <div class="bg-gray-50 border border-gray-200 rounded p-6">
             <div class="flex">
                 <img
