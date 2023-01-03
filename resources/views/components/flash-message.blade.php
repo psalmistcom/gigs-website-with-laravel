e492bdf60338317e73a3a94d57403f9d4dec8663
psalmistcom <dadewale9@gmail.com>
2023-01-03 19:18:40 +0100

    flash message adjusted

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,5 +1,5 @@
-@if (session()->has('message'))
-    <div class="fixed top 0 left-1/2 transform -translate-x-1/2 bg-laravel text-white px-48 py-3">
+@if(session()->has('message'))
+    <div class="fixed top-0 left-1/2 transform -translate-x-1/2 bg-green-800 text-white px-28 py-3">
         <p>
             {{session('message')}}
         </p>
