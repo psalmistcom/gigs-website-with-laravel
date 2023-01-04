a83d38af44c4fceae59e1acce7ba33179f7ea5b7
psalmistcom <dadewale9@gmail.com>
2023-01-04 12:19:54 +0100

    update done

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -54,4 +54,25 @@ class ListingController extends Controller
     {
         return view('listings.edit', ['listing' => $listing]);
     }
+
+    public function update(Request $request, Listing $listing)
+    {
+        // dd($request->file('logo'));
+        $formFields = $request->validate([
+            "title" => "required",
+            "company" => ["required"],
+            "email" => ["required", "email"],
+            "website" => "required",
+            "location" => "required",
+            "tags" => "required",
+            "description" => "required"
+        ]);
+
+        if ($request->hasFile('logo')) {
+            $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+        }
+
+        $listing->update($formFields);
+        return back()->with('message', 'Listing updated successfully');
+    }
 }
