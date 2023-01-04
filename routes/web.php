54418ec620558dca9e55edc86c7f5f0bb177bcf2
psalmistcom <dadewale9@gmail.com>
2023-01-04 14:21:11 +0100

    regiister user

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\ListingController;
 use App\Models\Listing;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ListingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -33,8 +34,13 @@ Route::post('/listings', [ListingController::class, 'store']);
 Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
 Route::put('/listings/{listing}', [ListingController::class, 'update']);
 Route::delete('/listings/{listing}', [ListingController::class, 'delete']);
-
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
+//user controller
+Route::get('/register', [UserController::class, 'create']);
+Route::post('/user', [UserController::class, 'store']);
+
+Route::get('/login', [UserController::class, 'login']);
 
 
 
